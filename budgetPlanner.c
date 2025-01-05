b0d96d0d0beb51c6864c599b70cbcd14f6bca459
Abu Tayeb Mohammad Ayon <mohammadayon2838@gmail.com>
2025-01-05 20:20:29 +0600

    Update budgetPlanner.c

--- budgetPlanner.c
+++ budgetPlanner.c
@@ -1,257 +1,215 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-struct budgetplan
-{
+struct budgetplan {
     float income;
     float savingsTarget;
     float food;
-    float accommodion;
+    float accommodation;
     float healthcare;
     float education;
     float clothing;
     float entertainment;
-    float totalexpenditure;
-    float differenceMoney;
+    float totalExpenditure;
+    float remainingMoney;
 };
 
-
-struct spendMoney
-{
+struct spendMoney {
     char nameItem[50];
     float cost;
 };
 
-/*struct calcMoney
-{
-    float remainingMoney;
-    float moneySpent;
-};*/
-
-
-float add(float x,float y);
+float add(float x, float y);
 float subs(float x, float y);
 
-void writeBudgetPlan(struct budgetplan s);
-void writeSpendMoney(struct spendMoney s[], int x);
-void readBudgetplan();
+void createBudgetPlan(struct budgetplan *b);
+void viewBudgetPlan(const struct budgetplan *b);
+void recordSpending(struct spendMoney s[], int *count, struct budgetplan *b);
+void viewSpendingSummary(struct spendMoney s[], int count);
+void saveBudgetPlanToFile(const struct budgetplan *b);
+void loadBudgetPlanFromFile(struct budgetplan *b);
+void saveSpendingToFile(struct spendMoney s[], int count);
+void loadSpendingFromFile(struct spendMoney s[], int *count);
 
+int main(void) {
+    struct budgetplan bp;
+    struct spendMoney spm[200];
+    int spendingCount = 0;
+    int option;
 
-int main(void)
-{
-    FILE *fp1;
-    struct budgetplan emp;
-    struct spendMoney spm1[200];
-    //struct calcMoney cm;
-    float remaingMoney, moneySpent;
-    int button1,button2,button3,button4,i=0,j,k=1;
+    printf("\n\t\t *** Welcome to the Budget Planner Application ***\n\n");
+    loadBudgetPlanFromFile(&bp);
+    loadSpendingFromFile(spm, &spendingCount);
 
+    do {
+        printf("1. Create a New Budget Plan\n");
+        printf("2. View Budget Plan\n");
+        printf("3. Record Spending\n");
+        printf("4. View Spending Summary\n");
+        printf("5. Save and Quit\n");
+        printf("Select an option (1-5): ");
+        scanf("%d", &option);
 
-    do
-    {
-        printf(" \n\t\t ***Welcome to our Budget System App***\n\n");
-        printf("1.Create new Budget Plan.\n");
-        printf("2.Work with existing budget plan.\n");
-        printf("3.Quit\n");
-        printf("Press 1,2 or 3 to execute the above-mentioned function:");
-        scanf("%d",&button1);
-        switch(button1)
-        {
-        case 1:
-            do
-            {
-                printf("\n\t\t***You Have Opted to Create a New Budget Plan***\n\n ");
-                printf("\nEnter  your income :");
-                scanf("%f",&emp.income);
-                printf("\nThe amount of money you wanted to save :");
-                scanf("%f",&emp.savingsTarget);
-                printf("\n\n\t\t***Allocate Budget in Different Sectors ***\n\n ");
-                printf("\nFood:");
-                scanf("%f",&emp.food);
-                printf("\nAccommodation:");
-                scanf("%f",&emp.accommodion);
-                printf("\nHealth care:");
-                scanf("%f",&emp.healthcare);
-                printf("\nEducation:");
-                scanf("%f",&emp.education);
-                printf("\nclothing:");
-                scanf("%f",&emp.clothing);
-                printf("\nEntertainment:");
-                scanf("%f",&emp.entertainment);
-
-                emp.differenceMoney = subs(emp.income,emp.savingsTarget);
-                emp.totalexpenditure = add(emp.food,emp.accommodion)+add(emp.healthcare,emp.education)+add(emp.clothing,emp.entertainment);
-
-                if(emp.totalexpenditure>emp.differenceMoney)
-                {
-                    printf("\n\tYour expenditure is too much !!!\n\tPlease re-enter the value!\n\tPress any digit to continue: ");
-                    scanf("%d",&button4);
-
-                }
-                else if(emp.differenceMoney>emp.totalexpenditure)
-                {
-                    emp.savingsTarget += subs(emp.differenceMoney,emp.totalexpenditure);
-                    printf("\nWith this budget plan you can save extra %.2f Tk. and your savings target will be %.2f Tk.",subs(emp.differenceMoney,emp.totalexpenditure),emp.savingsTarget);
-                    writeBudgetPlan(emp);
-
-                    printf("\nPress any digit to continue: ");
-                    scanf("%d",&button4);
-
-                }
-                else
-                {
-                    writeBudgetPlan(emp);
-                }
-
-            }while(emp.totalexpenditure>emp.differenceMoney);
-            break;
-        case 2:
-            do
-            {
-                printf("\n\n\t\t***You have opted to use existing budget plan***\n\n");
-                printf("1.View your budget plan.\n");
-                printf("2.Spend money and keep record.\n");
-                printf("3.View your list of spending.\n");
-                printf("4.Go to the main menu");
-                printf("\n\nPress 1,2,3 or 4 to execute the above-mentioned functions:");
-                scanf("%d",&button2);
-
-                if(button2==1)
-                {
-
-                    readBudgetplan();
-
-                }
-                else if(button2==2)
-                {
-
-                     do{
-                            printf("\n\n\t\t***You are now spending your Money.***\n\n");
-                            fflush(stdin);
-                            printf("Name of item you are spending money on:");
-                            gets(spm1[i].nameItem);
-                            fflush(stdin);
-                            printf("The cost of the item:");
-                            scanf("%f", &spm1[i].cost);
-                            printf("You have spent %.2f money on %s.\n",spm1[i].cost,spm1[i].nameItem);
-                            moneySpent += spm1[i].cost;
-                            printf("Your total money spent is %.2f\n", moneySpent);
-                            remaingMoney = emp.totalexpenditure - moneySpent;
-                            printf("Your remaining money is %.2f\n", remaingMoney);
-
-                            printf("Press 0 to go back or any other digit to continue:");
-                            scanf("%d",&button3);
-
-                            i++;
-
-                        }while(i<200 && button3!=0);
-
-                        writeSpendMoney(spm1, i);
-
-                }
-                /*else if(button2==3)
-                {
-
-                }
-                else if(button2==4)
-                {
-
-                }
-                else
-                {
-                    printf("\nInvalid Entry ! Try again\n!");
-                }*/
-
-            }while(button2!=4);
-
-            break;
-        case 3:
-
-            break;
-
-        default:
-
-            printf("Invalid Entry ! please try again!\n\n");
-
+        switch(option) {
+            case 1:
+                createBudgetPlan(&bp);
+                break;
+            case 2:
+                viewBudgetPlan(&bp);
+                break;
+            case 3:
+                recordSpending(spm, &spendingCount, &bp);
+                break;
+            case 4:
+                viewSpendingSummary(spm, spendingCount);
+                break;
+            case 5:
+                saveBudgetPlanToFile(&bp);
+                saveSpendingToFile(spm, spendingCount);
+                printf("Data saved successfully. Exiting the application. Goodbye!\n");
+                break;
+            default:
+                printf("Invalid option. Please try again.\n");
+                break;
         }
-
-    }while(button1!=3);
+    } while (option != 5);
 
     return 0;
 }
-float add(float x,float y)
-{
-    float z;
-    z=x+y;
-    return z;
-}
-float subs(float x,float y)
-{
-    float z;
-    if(x>y)
-    {
-        z=x-y;
-    }
-    else
-    {
-        z=y-x;
-    }
 
-    return z;
-
+float add(float x, float y) {
+    return x + y;
 }
-void writeBudgetPlan(struct budgetplan s)
-{
-    FILE *fptr;
-    fptr = fopen("BudgetPlan.txt", "w");
-    fprintf(fptr,"%.2f\n",s.income);
-    fprintf(fptr,"%.2f\n",s.savingsTarget);
-    fprintf(fptr,"%.2f\n",s.food);
-    fprintf(fptr,"%.2f\n",s.accommodion);
-    fprintf(fptr,"%.2f\n",s.healthcare);
-    fprintf(fptr,"%.2f\n",s.education);
-    fprintf(fptr,"%.2f\n",s.clothing);
-    fprintf(fptr,"%.2f\n",s.entertainment);
-    //fprintf(fptr,"%.2f\n",);
-    //fprintf(fptr,"%.2f\n",);
+
+float subs(float x, float y) {
+    return (x > y) ? (x - y) : (y - x);
+}
+
+void createBudgetPlan(struct budgetplan *b) {
+    printf("\n\t*** Creating a New Budget Plan ***\n");
+    printf("Enter your total income: ");
+    scanf("%f", &b->income);
+    printf("Enter your savings target: ");
+    scanf("%f", &b->savingsTarget);
+
+    printf("\nAllocate your budget in different categories:\n");
+    printf("Food: ");
+    scanf("%f", &b->food);
+    printf("Accommodation: ");
+    scanf("%f", &b->accommodation);
+    printf("Healthcare: ");
+    scanf("%f", &b->healthcare);
+    printf("Education: ");
+    scanf("%f", &b->education);
+    printf("Clothing: ");
+    scanf("%f", &b->clothing);
+    printf("Entertainment: ");
+    scanf("%f", &b->entertainment);
+
+    b->totalExpenditure = add(add(b->food, b->accommodation), add(add(b->healthcare, b->education), add(b->clothing, b->entertainment)));
+    b->remainingMoney = subs(b->income, b->totalExpenditure);
+
+    if (b->totalExpenditure > b->income - b->savingsTarget) {
+        printf("Warning: Your expenditure exceeds your target savings! Consider adjusting your plan.\n");
+    } else {
+        printf("Budget plan created successfully! Remaining money: %.2f\n", b->remainingMoney);
+    }
+}
+
+void viewBudgetPlan(const struct budgetplan *b) {
+    printf("\n\t*** Viewing Budget Plan ***\n");
+    printf("Total Income: %.2f\n", b->income);
+    printf("Savings Target: %.2f\n", b->savingsTarget);
+    printf("Food: %.2f\n", b->food);
+    printf("Accommodation: %.2f\n", b->accommodation);
+    printf("Healthcare: %.2f\n", b->healthcare);
+    printf("Education: %.2f\n", b->education);
+    printf("Clothing: %.2f\n", b->clothing);
+    printf("Entertainment: %.2f\n", b->entertainment);
+    printf("Total Expenditure: %.2f\n", b->totalExpenditure);
+    printf("Remaining Money: %.2f\n", b->remainingMoney);
+}
+
+void recordSpending(struct spendMoney s[], int *count, struct budgetplan *b) {
+    char cont;
+    do {
+        printf("\n\t*** Recording Spending ***\n");
+        printf("Enter the name of the item: ");
+        scanf("%s", s[*count].nameItem);
+        printf("Enter the cost of the item: ");
+        scanf("%f", &s[*count].cost);
+
+        b->remainingMoney -= s[*count].cost;
+        printf("You spent %.2f on %s. Remaining money: %.2f\n", s[*count].cost, s[*count].nameItem, b->remainingMoney);
+
+        (*count)++;
+
+        printf("Do you want to record another spending? (y/n): ");
+        scanf(" %c", &cont);
+    } while (cont == 'y' && *count < 200);
+}
+
+void viewSpendingSummary(struct spendMoney s[], int count) {
+    float totalSpent = 0;
+    printf("\n\t*** Spending Summary ***\n");
+    printf("%-20s %-10s\n", "Item", "Cost");
+    printf("--------------------------------\n");
+    for (int i = 0; i < count; i++) {
+        printf("%-20s %.2f\n", s[i].nameItem, s[i].cost);
+        totalSpent += s[i].cost;
+    }
+    printf("--------------------------------\n");
+    printf("Total Spent: %.2f\n", totalSpent);
+}
+
+void saveBudgetPlanToFile(const struct budgetplan *b) {
+    FILE *fptr = fopen("BudgetPlan.txt", "w");
+    if (fptr == NULL) {
+        printf("Error: Unable to save budget plan.\n");
+        return;
+    }
+    fprintf(fptr, "%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f\n", b->income, b->savingsTarget, b->food, b->accommodation, b->healthcare, b->education, b->clothing, b->entertainment, b->remainingMoney);
     fclose(fptr);
 }
-void writeSpendMoney(struct spendMoney s[],int x)
-{
-    FILE *fptr;
-    int i;
-    fptr = fopen("SpendMoney.txt", "a");
-    for(i=0;i<x;i++)
-    {
-        fprintf(fptr,"%s\n",s[i].nameItem);
-        fprintf(fptr,"%.2f\n",s[i].cost);
+
+void loadBudgetPlanFromFile(struct budgetplan *b) {
+    FILE *fptr = fopen("BudgetPlan.txt", "r");
+    if (fptr == NULL) {
+        printf("No existing budget plan found. Please create a new one.\n");
+        return;
     }
+    fscanf(fptr, "%f %f %f %f %f %f %f %f %f", &b->income, &b->savingsTarget, &b->food, &b->accommodation, &b->healthcare, &b->education, &b->clothing, &b->entertainment, &b->remainingMoney);
     fclose(fptr);
+    printf("Budget plan loaded successfully.\n");
 }
-void readBudgetplan()
-{
-    struct budgetplan r;
-    FILE *fptr;
-    fptr=fopen("BudgetPlan.txt","r");
-    printf("\n\n\t\t***This is your budget plan***");
-    fscanf(fptr,"%f", &r.income);
-    printf("\nTotal Income: %.2f", r.income);
-    fscanf(fptr,"%f",&r.savingsTarget);
-    printf("\nSaving target: %.2f", r.savingsTarget);
 
-    printf("\n\n\t\t***Budget allocation in different sectors***");
-    fscanf(fptr,"%f", &r.food);
-    printf("\n\nFood: %.2f", r.food);
-    fscanf(fptr,"%f", &r.accommodion);
-    printf("\nAccommodation:%.2f", r.accommodion);
-    fscanf(fptr,"%f", &r.healthcare);
-    printf("\nHealth care: %.2f", r.healthcare);
-    fscanf(fptr,"%f", &r.education);
-    printf("\nEducation: %.2f", r.education);
-    fscanf(fptr,"%f", &r.clothing);
-    printf("\nClothing: %.2f", r.clothing);
-    fscanf(fptr,"%f", &r.entertainment);
-    printf("\nEntertainment: %.2f", r.entertainment);
+void loadSpendingFromFile(struct spendMoney s[], int *count) {
+    FILE *fptr = fopen("SpendMoney.txt", "r");
+    if (fptr == NULL) {
+        printf("No previous spending records found.\n");
+        return;
+    }
+    
+    while (fscanf(fptr, "%s %f", s[*count].nameItem, &s[*count].cost) == 2) {
+        (*count)++;
+        if (*count >= 200) break;  // Ensure you don't exceed the array size
+    }
+
     fclose(fptr);
-
+    printf("Spending records loaded successfully.\n");
+}
+void saveSpendingToFile(struct spendMoney s[], int count) {
+    FILE *fptr = fopen("SpendMoney.txt", "w");
+    if (fptr == NULL) {
+        printf("Error: Unable to save spending records.\n");
+        return;
+    }
+    
+    for (int i = 0; i < count; i++) {
+        fprintf(fptr, "%s %.2f\n", s[i].nameItem, s[i].cost);
+    }
+    
+    fclose(fptr);
+    printf("Spending records saved successfully.\n");
 }
